9b64a8906b89afb4e79f2edb2465ea05c4c14f5a
Nathan Begle <ntbegle@gmail.com>
2018-10-27 11:26:15 -0400

    Updated parameters and test name

--- src/ArraysAndStrings/Is_Unique.py
+++ src/ArraysAndStrings/Is_Unique.py
@@ -2,19 +2,19 @@
 #implement an algorithm to determine if string has all unique characters
 #input is a string
 #output is true if string is unique, false otherwise
-def is_unique(str):
+def is_unique(s):
     charDict = dict()
-    for c in str:
+    for c in s:
         if c in charDict:
             return False
         else:
             charDict[c] = True
     return True
 
-def is_unique_no_ds(str):
-    for i in range(0,len(str)-1):
-        for j in range(i+1,len(str)):
-            if str[i] == str[j]:
+def is_unique_no_ds(s):
+    for i in range(0,len(s)-1):
+        for j in range(i+1,len(s)):
+            if s[i] == s[j]:
                 return False
 
 

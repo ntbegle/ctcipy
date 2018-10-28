ad2d196d653774bba4cb7feba3770e8615875b94
Nathan Begle <ntbegle@gmail.com>
2018-10-28 14:45:08 -0400

    Added new function to check if 2 strings are permutations of each other.

---
+++ src/ArraysAndStrings/Check_Permutation.py
@@ -0,0 +1,41 @@
+
+#possible solutions
+#   1. brute force by looping over chars in str1 and seeing if it exists in str2
+#       need to make sure duplicates are captured.  this can be done by creating a marker array
+#       to mark when a letter has been used
+#   2. sort strings and them loop through both at the same time and check that the chars match
+#       this won't take into account equal strings (is that a permutation???)
+#       this will have O(nlogn) time complexity
+#   3. create dictionaries of chars in the strings, one dict for each string.  make sure count of
+#       chars in dictionaries match.
+#       3 total loops giving time complexity of O(n)
+#   4. another way I read about online is to only use one dict.  Increment value by 1 if it exists in str1
+#       decrement if it exists in str2.  final values in dict should all be 0
+
+# use dictionaries to count the letters in each string
+def isPermutation(str1, str2):
+    if len(str1) != len(str2):
+        return False
+
+    dict1 = dict()
+    dict2 = dict()
+
+    for i in range(0,len(str1)):
+        if str1[i] in dict1:
+            dict1[str1[i]] += 1
+        else:
+            dict1[str1[i]] = 1
+
+        if str2[i] in dict2:
+            dict2[str2[i]] += 1
+        else:
+            dict2[str2[i]] = 1
+
+    for i in range(0, len(str1)):
+        if str1[i] in dict2:
+            if dict1[str1[i]] != dict2[str1[i]]:
+                return False
+        else:
+            return False
+
+    return True
---
+++ test/ArraysAndStrings_ut/isPermutation_ut.py
@@ -0,0 +1,25 @@
+import unittest
+import ArraysAndStrings.Check_Permutation
+
+class MyTestCase(unittest.TestCase):
+    def test_not_permutations(self):
+        str1 = ['abcde', 'hello world', 'qwerty']
+        str2 = ['abcdf', 'dlrowolleh', 'qwertyu']
+
+        for i in range(0, len(str1) - 1):
+            result = ArraysAndStrings.Check_Permutation.isPermutation(str1[i], str2[i])
+            self.assertFalse(result, 'Error: ' + str1[i] + ' is not a permuation of ' + str2[i])
+
+    def test_permutations(self):
+        str1 = ['abcde', 'hello world', 'qwerty']
+        str2 = ['abcde', 'dlrow olleh', 'ywertq']
+
+        for i in range(0, len(str1) - 1):
+            result = ArraysAndStrings.Check_Permutation.isPermutation(str1[i], str2[i])
+            self.assertTrue(result, 'Error: ' + str1[i] + ' is a permutation of ' + str2[i])
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
